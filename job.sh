f8f7bf6858dba7b2492d7b2406bf9e1525b6ec60
Guowei He <gh50@nyu.edu>
2018-06-05 14:31:24 +0400

    remove comment

--- job.sh
+++ job.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
-#SBATCH -a 1-100
+#SBATCH -a 2-18
 #SBATCH -n 1
 #SBATCH --cpus-per-task=1
 #SBATCH --time=48:00:00
-
-### POSSIBLY NEED TO SOURCE .bashrc TO GET LIBRARY PATHS ###
 
 # Path to list of input commands
 commands="./commands"
@@ -16,4 +14,4 @@ awk "NR == $SLURM_ARRAY_TASK_ID {print; exit}" ${commands} > ${command_list}
 echo "Doing line ${SLURM_ARRAY_TASK_ID}, $(cat ${command_list})"
 source ${command_list}
 echo "Finished line ${SLURM_ARRAY_TASK_ID}"
-rm -rf ${command_list}
+rm "${command_list}"
